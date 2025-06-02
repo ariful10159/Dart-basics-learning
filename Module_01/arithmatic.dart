e2335e53438e114714e2ac2602100c9f2da08900
Ariful islam Masum <arifulislammasum97@gmail.com>
2025-06-03 03:15:56 +0600

    Add files via upload

---
+++ Module_01/arithmatic.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 15;
+  int b = 4;
+
+  print('a + b = ${a + b}');  // Addition
+  print('a - b = ${a - b}');  // Subtraction
+  print('a * b = ${a * b}');  // Multiplication
+  print('a / b = ${a / b}');  // Division (returns double)
+  print('a ~/ b = ${a ~/ b}'); // Integer Division
+  print('a % b = ${a % b}');  // Modulus (Remainder)
+}
