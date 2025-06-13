ec7daf87401664f0ac9040099d569b62aacd4be3
Ariful islam Masum <arifulislammasum97@gmail.com>
2025-06-13 21:53:24 +0600

    Add files via upload

---
+++ Module 02/2.1_set_add.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  Set<int> numSet = {1, 2, 3};
+
+  var mycities = <String>{'Dhaka','Cumilla','Chattogram'};
+
+  print(mycities);
+
+  mycities.add('khulna');
+  print(mycities);
+
+  mycities.addAll({'barisal','patuakhali'});
+
+  print(mycities);
+
+  //duplicate value not allow
+
+
+}
